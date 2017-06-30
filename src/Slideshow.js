5b1ff7db2a9b6aab7f4fa0d1217ffdc057f28d74
Angelique Lazarus <javathehut@users.noreply.github.com>
2017-06-30 18:48:56 -0400

    slideshow working but not yet automatic

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -6,16 +6,29 @@ class Slideshow extends Component {
     constructor() {
         super();
         this.state = {
-            imgIndex: 0
+            imgIndex: 0,
         }
         this.changePhoto = this.changePhoto.bind(this);
+        this.carousel = this.carousel.bind(this);
     }
 
-    changePhoto(num) {
-        this.setState({
-            imgIndex: this.state.imgIndex + num
+    changePhoto() {
+        setTimeout(this.carousel(), 2000);
+    }
+
+    carousel() {
+        for (let i = 0; i < homeImages.length; i ++){
+            this.setState({
+            imgIndex: this.state.imgIndex + 1
         })
     }
+    
+        if (this.state.imgIndex == homeImages.length-1) { 
+            this.setState({
+            imgIndex: 0
+        }) }
+    }
+
 
 
     render() {
@@ -24,12 +37,14 @@ class Slideshow extends Component {
         return (
             <div>
                 <div>
+                    <button onClick={() => { this.changePhoto() }}>start</button>
+                    <br />
                     <img className="slideshowImg" src={homeImages[imgIndex]} />
                 </div>
                 <div>
                     <button onClick={() => { this.changePhoto(-1) }} disabled={imgIndex == 0 ? "true" : ""} >Previous</button>
                     <span>{imgIndex + 1} of {homeImages.length}</span>
-                    <button onClick={() => { this.changePhoto(1) }} disabled={imgIndex == homeImages.length-1 ? "true" : ""} >Next</button>
+                    <button onClick={() => { this.changePhoto(1) }} disabled={imgIndex == homeImages.length - 1 ? "true" : ""} >Next</button>
                 </div>
             </div>
         );
